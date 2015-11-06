47330ed4af4fbfb222a3e5f1abc26fca5e8b970d
Sandjiv Parassouramanaick <sandjiv.parassou@gmail.com>
2015-11-06 20:45:30 +0100

    JE SUIS UNE CLASSE AVEC UN MAIIIIN!!!

---
+++ src/com/company/Serveur.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Sandjiv on 06/11/2015.
+ */
+public class Serveur {
+
+        public static void main(String[] args) {
+            // write your code here
+            System.out.println("je suis un serveur !!");
+        }
+    }
+
